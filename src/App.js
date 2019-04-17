e836830691054ad928ece8d9bb2b8f74cc92d732
jhonrsalcedo <jhonfor@gmail.com>
2019-04-17 11:13:58 -0500

    Guardando noticias en State

--- src/App.js
+++ src/App.js
@@ -3,6 +3,47 @@ import Header from './Components/Header';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    //no se puede llamar en este bloque this.setState()
+    this.state = {
+      newsResponse:[]
+    }
+  }
+
+  //recordar: Este método solo se ejecuta justo después de que el componente haya sido montado en el DOM.
+  componentWillMount(){
+    //llamamos al metodo
+    this.checkNews();
+
+  }
+  // Creamos una funcion para consultar la api
+  checkNews =()=>{
+    let url = 'https://newsapi.org/v2/top-headlines?country=co&category=general&apiKey=27c0e34cbbb3476a8b67c4cb2e666a37';
+    
+    //console.log(url)
+
+    //nota: Con ECMAScript6 tenemos acceso a una nueva función llamada fetch que a través de Promesas nos simplifica y facilita el realizar una petición AJAX de forma nativa.
+    fetch(url)
+    //recibimos una repuesta del servidor
+    .then(response =>{
+      //funcion json() para pasar esa respuesta a json
+      return response.json();
+    })
+    //luego recibimos las noticias 
+    .then(newsResponse =>{
+      
+      //esta respuesta(en arreglos) se debe llevar al state
+      console.log(newsResponse.articles);
+      // mandamos las respuesta de noticias por medio de this.setState()
+      this.setState({
+        newsResponse: newsResponse.articles
+      })
+
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,8 @@ const Header =(props) =>{
     return(
         <nav>
             <div className="nav-wrapper light-blue darken-3">
-        <a className="brand-logo center">{props.title}</a></div>
+        <a href="https://github.com" className="brand-logo center">{props.title} </a>
+        </div>
         </nav>
     );
 };
