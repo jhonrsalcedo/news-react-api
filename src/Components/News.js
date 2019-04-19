7baa41be9880ae826d04c75ce521f7ae1c033256
jhonrsalcedo <jhonfor@gmail.com>
2019-04-19 11:44:52 -0500

    agregando efectos

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,21 +1,31 @@
 import React, {Component} from 'react';
 import Onenews from './Onenews';
 import PropTypes from 'prop-types';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+
 
 //componente de noticias
 class News extends Component{
     render(){
         return(
             <div className="row">
-
+            <TransitionGroup>
                 {this.props.news.map(allnews =>(
-                    //con map hacemos el recorido de las noticias y los pasamos al componente Onenews
+//con map hacemos el recorido de las noticias y los pasamos al componente Onenews
+                    <CSSTransition 
+                    key={allnews.url}
+                    classNames="fade"
+                    timeout={500}
+                    >
+                    
                     <Onenews
                     //debemos identificar un key y para esto utilizaremos la url que seria unica en cada noticias
-                        key={allnews.url}
+                       
                         allnews={allnews}
                     />
+                    </CSSTransition>
                 ))}
+                </TransitionGroup>
             </div>
         );
     };
